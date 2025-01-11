923e827be698f1a4c83a739be332f7d3bd60e619
codewithhasnain <163335665+codewithhasnain@users.noreply.github.com>
2025-01-12 02:05:06 +0500

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ import express from "express"
 const app = express()
 
 app.use(express.static("static"))
-app.listen(4550, () => {
-  console.log("listening on port http://localhost:4550")
+app.listen(4551, () => {
+  console.log("listening on port http://localhost:4551")
 })
